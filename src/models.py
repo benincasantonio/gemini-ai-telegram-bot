d7f3953e6c9964fbf4af3bee65fc48eb49a4d8bf
Antonio Benincasa <benincasaantonio98@gmail.com>
2024-11-17 13:52:55 +0100

    feat: add db models

---
+++ src/models.py
@@ -0,0 +1,13 @@
+from telegram_bot_api import db
+
+class ChatMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.Integer)
+    text = db.Column(db.Text)
+    date = db.Column(db.DateTime)
+
+class ChatSession(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.Integer)
+    messages = db.relationship('ChatMessage', backref='chat_session')
+    
