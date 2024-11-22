5475f212d5cb5e54d15d373b063ec8932202bcc1
Antonio Benincasa <benincasaantonio98@gmail.com>
2024-11-22 07:24:09 +0100

    fix: db uri env name

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(32))
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///db.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
