d6fe7e258d6eb967d4f6212c640832b5767bcf72
Antonio Benincasa <benincasaantonio98@gmail.com>
2024-12-28 07:05:17 +0100

    feat: refine secure webhook token validation logic and improve logging

--- src/telegram_bot_api.py
+++ src/telegram_bot_api.py
@@ -22,13 +22,18 @@ async def webhook():
     telegram_app = ApplicationBuilder().token(getenv('TELEGRAM_BOT_TOKEN')).build()
     gemini = Gemini()
 
-    enable_secure_webhook_token = getenv('ENABLE_SECURE_WEBHOOK_TOKEN') == 'True'
+    """ enable_secure_webhook_token = getenv('ENABLE_SECURE_WEBHOOK_TOKEN') == 'True' if True else False """
+
+    enable_secure_webhook_token = getenv('ENABLE_SECURE_WEBHOOK_TOKEN') in ('True', None)
+
+    print("Enable secure webhook token: ", enable_secure_webhook_token)
 
     if enable_secure_webhook_token:
         headers_secret_token = request.headers.get('X-Telegram-Bot-Api-Secret-Token')
         secret_token = getenv('TELEGRAM_WEBHOOK_SECRET')
         if headers_secret_token != secret_token or headers_secret_token is None:
             await telegram_app.bot.send_message(chat_id=chat_id, text="Invalid secret token")
+            return 'OK'
     
 
     
