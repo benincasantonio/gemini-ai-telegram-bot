1882992d3ed7dee29b5a88b2dd898594fd3bcac4
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-07-05 16:11:40 +0200

    fix: update return statement in send_message method to access output correctly

--- src/gemini.py
+++ src/gemini.py
@@ -70,7 +70,7 @@ class Gemini:
         # if(function_response.text == None):
         #     return "I'm sorry, An error occurred. Please try again."
 
-        return invoke_response.output
+        return invoke_response['output']
 
     def send_image(self, prompt: str, image: PIL.Image):
         response = self.__model.generate_content([prompt, image])
