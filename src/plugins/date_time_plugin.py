99231b467b622b90885f7eae79803c983dc11705
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-07-05 16:50:19 +0200

    fix: refactor DateTimePlugin to use Pydantic for argument validation and remove old parameter structure

--- src/plugins/date_time_plugin.py
+++ src/plugins/date_time_plugin.py
@@ -1,32 +1,30 @@
 from datetime import datetime
+from pydantic import BaseModel, Field
 from pytz import timezone
 from langchain_core.tools import Tool
 from langchain import hub
 
+
+class DateTimeArgSchema(BaseModel):
+    time_zone: str = Field(
+        default="Europe/Rome",
+        description="The timezone to use for the date and time.",
+        examples=["Europe/Rome", "America/New_York", "Asia/Tokyo"]
+    )
 
 class DateTimePlugin:
 
     def __init__(self):
         self.__name: str = "get_date_time"
         self.__description: str = "A plugin that returns the current date and time."
-        self.__parameters: dict[str, any] = {
-            "type": "object",
-            "properties": {
-                "time_zone": {
-                    "type": "string",
-                    "description": "The timezone to use for the date and time."
-                }
-            }
-        }
 
     def get_tool(self) -> Tool:
         return Tool(
             name=self.__name,
             description=self.__description,
-            func=DateTimePlugin.get_date_time
+            func=self.get_date_time,
         )
 
     @staticmethod
     def get_date_time(time_zone="Europe/Rome") -> str:
-        print('TIMEZONE', time_zone)
         return datetime.now(timezone(time_zone)).strftime("%Y-%m-%d %H:%M:%S")
