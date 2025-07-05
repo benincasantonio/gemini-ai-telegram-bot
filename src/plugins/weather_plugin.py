b3f6a43d005a6249a7dfa7b9a5d8a81cf6b62517
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-07-05 16:35:29 +0200

    fix: refactor WeatherPlugin to use Pydantic for argument validation and improve tool structure

--- src/plugins/weather_plugin.py
+++ src/plugins/weather_plugin.py
@@ -1,71 +1,52 @@
+from pydantic import BaseModel, Field
 from pyowm import OWM
 from dotenv import load_dotenv
 from os import getenv
 from datetime import datetime
 import dateparser
-from langchain_core.tools import Tool, tool
+from langchain_core.tools import Tool, tool, StructuredTool
 
 load_dotenv()
 owm = OWM(getenv('OWM_API_KEY'))
 
+class WeatherArgSchema(BaseModel):
+    city: str = Field(
+        description="The city name.",
+        examples=["Rome", "New York", "Tokyo"]
+    )
+    date_time: str = Field(
+        default=datetime.now().strftime('%d-%m-%Y'),
+        description="The datetime timestamp for the weather. If not specified, the current date and time will be used.",
+        examples=["2023-10-01 14:00:00", "tomorrow", "in 2 days", "today"],
+    )
+    unit: str = Field(
+        default='celsius',
+        description="The unit of temperature. If not specified, the temperature will be returned in Celsius.",
+        examples=["celsius", "fahrenheit"],
+        enum=["celsius", "fahrenheit"]
+    )
+
+
 class WeatherPlugin: 
     def __init__(self):
-        self.name: str = "get_weather"
-        self.description: str = "Get the weather of a city at a particular date and time. If the date is today, the current weather will be returned. Otherwise, the weather at the specified date and time will be returned. If the user does not specify a date and time, the current date and time will be used. If the user types a date like 'tomorrow' or 'in 2 days', you should convert it to the appropriate date. If the user does not specify a unit, the temperature will be returned in Celsius. If the user specifies a unit, the temperature will be returned in that unit."
-        self.parameters: dict[str, any] = {
-            "type": "object",
-            "properties": {
-                "city": {
-                    "type": "string",
-                    "description": "The city name."
-                },
-                "date_time": {
-                    "type": "string",
-                    "description": "The datetime timestamp for the weather."
-                },
-                "unit": {
-                    "type": "string",
-                    "description": "The unit of temperature",
-                    "enum": ["celsius", "fahrenheit"]
-                }
-            }
-        }
-
+        self.__name: str = "get_weather"
+        self.__description: str = "Get the weather of a city at a particular date and time. If the date is today, the current weather will be returned. Otherwise, the weather at the specified date and time will be returned. If the user does not specify a date and time, the current date and time will be used. If the user types a date like 'tomorrow' or 'in 2 days', you should convert it to the appropriate date. If the user does not specify a unit, the temperature will be returned in Celsius. If the user specifies a unit, the temperature will be returned in that unit."
+        
     
     def get_tool(self) -> Tool:
-        return Tool(
-            name=self.name,
-            description=self.description,
-            func=WeatherPlugin.get_weather
+        return StructuredTool.from_function(
+            name=self.__name,
+            description=self.__description,
+            func=self.get_weather,
+            args_schema=WeatherArgSchema,
+            return_direct=True,
         )
     
 
-    @staticmethod
-    @tool(
-        name="get_weather",
-        description="Get the weather of a city at a particular date and time. If the date is today, the current weather will be returned. Otherwise, the weather at the specified date and time will be returned. If the user does not specify a date and time, the current date and time will be used. If the user types a date like 'tomorrow' or 'in 2 days', you should convert it to the appropriate date. If the user does not specify a unit, the temperature will be returned in Celsius. If the user specifies a unit, the temperature will be returned in that unit.",
-        args_schema={
-            "type": "object",
-            "properties": {
-                "city": {
-                    "type": "string",
-                    "description": "The city name."
-                },
-                "date_time": {
-                    "type": "string",
-                    "description": "The datetime timestamp for the weather. If not specified, the current date and time will be used. 'today', 'tomorrow', 'in 2 days' are accepted and similar expressions will be converted to the appropriate date.",
-                    "default": datetime.now().strftime('%d-%m-%Y')
-                },
-                "unit": {
-                    "type": "string",
-                    "description": "The unit of temperature. If not specified, Celsius will be used.",
-                    "enum": ["celsius", "fahrenheit"],
-                    "default": 'celsius'
-                }
-            }
-        }
-    )
     def get_weather(city: str, date_time: str = datetime.now().strftime('%d-%m-%Y'), unit: str = 'celsius') -> str:
+        print("City: " + city)
+        print("Unit: " + unit)
+        print("Date and time: " + date_time)
         mgr = owm.weather_manager()
 
         print("DATE: " + date_time)
