5819e7fce4f0913c8febf94b963f607ad22ccb28
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-07-06 15:46:19 +0200

    fix: update DateTimeArgSchema and DateTimePlugin descriptions for clarity

--- src/plugins/date_time_plugin.py
+++ src/plugins/date_time_plugin.py
@@ -5,23 +5,26 @@ from langchain_core.tools import StructuredTool
 
 
 class DateTimeArgSchema(BaseModel):
+    """Schema for the arguments of the get_date_time function. Should not be passed as dictionary, but as keyword arguments."""
     time_zone: str = Field(
         default="Europe/Rome",
         description="The timezone to use for the date and time. E.g. Europe/Rome, America/New_York, Asia/Tokyo.",
-        examples=["Europe/Rome", "America/New_York", "Asia/Tokyo"]
+        examples=["Europe/Rome", "America/New_York", "Asia/Tokyo"],
+        title="Time Zone",
     )
 
 class DateTimePlugin:
 
     def __init__(self):
         self.__name: str = "get_date_time"
-        self.__description: str = "A plugin that returns the current date and time."
+        self.__description: str = "A plugin that returns the current date and time. Pass the data as keyword arguments, not as a dictionary. The time zone can be specified using the 'time_zone' argument. If not specified, the default time zone is Europe/Rome. The time zone should be in the format 'Region/City', e.g. 'Europe/Rome', 'America/New_York', 'Asia/Tokyo'."
 
     def get_tool(self) -> StructuredTool:
         return StructuredTool(
             name=self.__name,
             description=self.__description,
             func=get_date_time,
+            args_schema=DateTimeArgSchema,
             return_direct=True,
         )
 
